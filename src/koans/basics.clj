841505fe96008875f69f3ec157b236ddd173ed72
Connor J Park <NQ311ZA@US.NA.EY.NET>
2018-07-09 14:00:07 -0400

    second commit

--- src/koans/basics.clj
+++ src/koans/basics.clj
@@ -8,56 +8,56 @@
  "If one falls, all fall."
  (= (run* [q]
       u#
-      (== q true)) __)
+      (== q true)) ())
 
  "If they all stand, we succeed."
  (= (run* [q]
       s#
-      (== q true)) __)
+      (== q true)) '(true))
 
  "What you want, is what you get."
  (= (run* [q]
-      (== q :logic-fun)) __)
+      (== q :logic-fun)) '(:logic-fun))
 
  "Those that are not equal, do not talk to each other."
  (= (run* [x]
       (let [x false]
-        (== true x))) __)
+        (== true x))) ())
 
  "New values are fresh, and juicy, and behave as others."
  (= (run* [q]
       (fresh [x]
         (== true x)
-        (== true q))) __)
+        (== true q))) '(true))
 
  "Unbound success is infinite, yet, representable."
- (= (run* [q] s#) __)
+ (= (run* [q] s#) '(_0))
 
  "Infinity twice is two different infinities."
  (= (run* [q] (fresh [x y]
-                (== q [x y]))) __)
+                (== q [x y]))) '([_0 _1]))
 
  "However, the same infinity remains the same when referred to twice."
  (= (run* [q] (fresh [x y]
-                (== q [x y x]))) __)
+                (== q [x y x]))) '([_0 _1 _0]))
 
  "Something which is, cannot be something else at the same time."
  (= (run* [q]
       (== q true)
-      (== q false)) __)
+      (== q false)) ())
 
  "Something which is, may be itself again."
  (= (run* [q]
       (== q false)
-      (== q false)) __)
+      (== q false)) '(false))
 
  "Something which is, is transitively so."
  (= (run* [q]
       (let [x q]
-        (== x true))) __)
+        (== x true))) '(true))
 
  "Something which is, is transitively so, when fresh, too."
  (= (run* [q]
       (fresh [x]
         (== q x)
-        (== true x))) __))
+        (== true x))) '(true)))
